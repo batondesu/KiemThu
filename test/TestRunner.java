46757e26cb8332bf013dee59915f68c392775f8b
batondesu <trantoan29072003@gmail.com>
2024-03-24 00:14:09 +0700

    ver4

---
+++ test/TestRunner.java
@@ -0,0 +1,18 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Axis.class);
+        
+        if (result.wasSuccessful()) {
+            System.out.println("Tất cả các testcase đã chạy thành công!");
+        } else {
+            System.out.println("Có lỗi xảy ra trong quá trình kiểm thử:");
+            for (Failure failure : result.getFailures()) {
+                System.out.println(failure.toString());
+            }
+        }
+    }
+}
